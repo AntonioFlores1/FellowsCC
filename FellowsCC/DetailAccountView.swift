10edfab946f4dd91e1d0be321a663a410a5cda2b
MattHuie <matthuie93@gmail.com>
2019-03-28 09:54:34 -0400

    fixed firebase user.uid and user can save and see it on their profile

--- FellowsCC/DetailAccountView.swift
+++ FellowsCC/DetailAccountView.swift
@@ -10,6 +10,121 @@ import UIKit
 
 class DetailAccountView: UIView {
 
+    lazy var profileImage: UIButton = {
+        let image = UIButton()
+        return image
+    }()
     
+    lazy var displayName: UITextField = {
+        let displayName = UITextField()
+        displayName.backgroundColor = .red
+        return displayName
+    }()
+    
+    lazy var firstName: UITextField = {
+        let firstName = UITextField()
+        firstName.backgroundColor = .blue
+        return firstName
+    }()
+    
+    lazy var lastName: UITextField = {
+        let lastName = UITextField()
+        lastName.backgroundColor = .blue
+        return lastName
+    }()
+    
+    lazy var bioTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .purple
+        return textView
+    }()
+    
+    lazy var doneButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .orange
+        return button
+    }()
 
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        backgroundColor = .white
+        setupView()
+    }
+}
+
+extension DetailAccountView {
+    private func setupView() {
+        setupProfileImage()
+        setupDisplayName()
+        setupFirstName()
+        setupLastName()
+        setupBio()
+        setupDoneButton()
+        
+    }
+    
+    private func setupProfileImage() {
+        addSubview(profileImage)
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        profileImage.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        profileImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        profileImage.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        profileImage.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
+    private func setupDisplayName() {
+        addSubview(displayName)
+        displayName.translatesAutoresizingMaskIntoConstraints = false
+        displayName.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 8).isActive = true
+        displayName.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        displayName.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        displayName.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    
+    private func setupFirstName() {
+        addSubview(firstName)
+        firstName.translatesAutoresizingMaskIntoConstraints = false
+        firstName.topAnchor.constraint(equalTo: displayName.bottomAnchor, constant: 8).isActive = true
+        firstName.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        firstName.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        firstName.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    
+    private func setupLastName() {
+        addSubview(lastName)
+        lastName.translatesAutoresizingMaskIntoConstraints = false
+        lastName.topAnchor.constraint(equalTo: firstName.bottomAnchor, constant: 8).isActive = true
+        lastName.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        lastName.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        lastName.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    
+    private func setupBio() {
+        addSubview(bioTextView)
+        bioTextView.translatesAutoresizingMaskIntoConstraints = false
+        bioTextView.topAnchor.constraint(equalTo: lastName.bottomAnchor, constant: 8).isActive = true
+        bioTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        bioTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        bioTextView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    }
+    
+    private func setupDoneButton() {
+        addSubview(doneButton)
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
+        doneButton.topAnchor.constraint(equalTo: bioTextView.bottomAnchor, constant: 20).isActive = true
+        doneButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        doneButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        doneButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
 }
--- FellowsCC/DetailAccountViewController.swift
+++ FellowsCC/DetailAccountViewController.swift
@@ -7,15 +7,112 @@
 //
 
 import UIKit
+import Toucan
 
 class DetailAccountViewController: UIViewController {
 
+    let detailView = DetailAccountView()
+    var userProfile: CCUser?
+    private var authservice = AppDelegate.authservice
+    private var selectedImage: UIImage?
+    private lazy var imagePickerController: UIImagePickerController = {
+        let ip = UIImagePickerController()
+        ip.delegate = self
+        return ip
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(detailView)
+        detailView.doneButton.addTarget(self, action: #selector(doneButtonPressed), for: .touchUpInside)
+        detailView.profileImage.addTarget(self, action: #selector(profileButtonPressed), for: .touchUpInside)
+        updateUI()
+        
 
     }
+    @objc func profileButtonPressed() {
+        var actionTitles = [String]()
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            actionTitles = ["Photo Library", "Camera"]
+        } else {
+            actionTitles = ["Photo Library"]
+        }
+        showActionSheet(title: nil, message: nil, actionTitles: actionTitles, handlers: [{ [unowned self] photoLibraryAction in
+            self.imagePickerController.sourceType = .photoLibrary
+            self.present(self.imagePickerController, animated: true)
+            }, { cameraAction in
+                self.imagePickerController.sourceType = .camera
+                self.present(self.imagePickerController, animated: true)
+            }
+            ])
+    }
+    
+    @objc func doneButtonPressed() {
+        guard let profileImage = detailView.profileImage.imageView?.image?.jpegData(compressionQuality: 1.0),
+            let firstName = detailView.firstName.text, !firstName.isEmpty,
+            let lastName = detailView.lastName.text, !lastName.isEmpty,
+            let displayName = detailView.displayName.text, !displayName.isEmpty,
+            let bio = detailView.bioTextView.text, !bio.isEmpty,
+            let user = authservice.getCurrentUser()
+            else {return}
+        StorageService.postImage(imageData: profileImage, imageName: "profileImages/" + user.uid) { [weak self] (error, imageURL) in
+            if let error = error {
+                self?.showAlert(title: "Error Saving Profile Photo", message: error.localizedDescription)
+            } else if let imageURL = imageURL {
+                DBService.firestoreDB.collection(UsersCollectionKeys.CollectionKey)
+                    .document(user.uid)
+                    .updateData([UsersCollectionKeys.PhotoURLKey : imageURL.absoluteString], completion: { (error) in
+                        if let error = error {
+                            self?.showAlert(title: "Error Saving Account Info", message: error.localizedDescription)
+                        }
+                    })
+            }
+            DBService.firestoreDB.collection(UsersCollectionKeys.CollectionKey)
+                .document(user.uid)
+                .updateData([UsersCollectionKeys.FirstNameKey : firstName,
+                             UsersCollectionKeys.LastNameKey : lastName,
+                             UsersCollectionKeys.DisplayNameKey: displayName,
+                             UsersCollectionKeys.BioKey: bio], completion: { (error) in
+                                if let error = error {
+                                    self?.showAlert(title: "Error Saving Account Info", message: error.localizedDescription)
+                                }
+                })
+        }
+        showAlert(title: "Success", message: "Successfully saved account info")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func updateUI() {
+        if let userProfile = userProfile {
+            detailView.profileImage.kf.setImage(with: URL(string: userProfile.photoURL!), for: .normal)
+            detailView.firstName.text = userProfile.firstName
+            detailView.lastName.text = userProfile.lastName
+            detailView.displayName.text = userProfile.displayName
+            detailView.bioTextView.text = userProfile.bio
+        }
+    }
+    
+    
     
 
     
 
+}
+extension DetailAccountViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            print("original image not available")
+            return
+        }
+        let size = CGSize(width: 500, height: 500)
+        let resizedImage = Toucan.Resize.resizeImage(originalImage, size: size)
+        selectedImage = resizedImage
+        detailView.profileImage.setImage(resizedImage, for: .normal)
+        dismiss(animated: true)
+    }
 }
